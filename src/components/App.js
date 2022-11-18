f3f09caa72f816884b79cbd3e6580bd9ed4adb6d
Harold_Mesa <harold.mesa@icloud.com>
2022-11-18 12:10:37 -0600

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import CoffeeShopControl from "./CoffeeShopControl";
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <CoffeeShopControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
