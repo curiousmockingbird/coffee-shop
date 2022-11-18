443eb70af10f48edd31c36e44ae60c58d57c2abf
Harold_Mesa <harold.mesa@icloud.com>
2022-11-18 13:00:02 -0600

    WIP: Working on CoffeeShopControl.js. Add App components to components folder

---
+++ src/components/CoffeeShopControl.js
@@ -0,0 +1,5 @@
+import React from "react";
+import CoffeeShopMenu from "./CoffeeShopMenu";
+import SackDetails from "./SackDetails";
+import NewSackForm from "./NewSackForm";
+import EditSackForm from "./EditSackForm.js";
