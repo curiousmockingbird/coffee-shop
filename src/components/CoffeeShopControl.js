ecc4c0818ebf3912ade08d8d025b68d545678464
Harold_Mesa <harold.mesa@icloud.com>
2022-11-18 13:39:56 -0600

    WIP: Working on custom methods for CoffeShopControl

--- src/components/CoffeeShopControl.js
+++ src/components/CoffeeShopControl.js
@@ -3,3 +3,30 @@ import CoffeeShopMenu from "./CoffeeShopMenu";
 import SackDetails from "./SackDetails";
 import NewSackForm from "./NewSackForm";
 import EditSackForm from "./EditSackForm.js";
+
+class CoffeeShopControl extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainSackList: [],
+      selectedSack: null,
+      editing: null
+    };
+  }
+
+  handleClick(){
+
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null
+    return(
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    )
+  }
+}
